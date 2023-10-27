098038fbb9ba5856fe0a560db5665864479408f6
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-26 22:38:00 -0500

    edited TODOs

--- chapter-5/conditionals.js
+++ chapter-5/conditionals.js
@@ -14,7 +14,7 @@
 console.log("\nThe Boolean Data Type");
 
 
-// TODO: Initialize a few booleans, then print them
+// Initialize a few booleans, then print them
 let hasFluxCapacitor = true;
 let hasPowerSource = false;
 
@@ -25,7 +25,7 @@ console.log(hasPowerSource);
 /** OTHER TYPES CONVERTED TO BOOLEAN **/
 console.log("\nOther Types Converted to Booleans");
 
-// TODO: Convert these strings and numbers to booleans and print the results
+// Convert these strings and numbers to booleans and print the results
 let scientist = "Doc Brown"; // truthy
 let speechless = ""; // falsy
 let jigawattsNeeded = 1.21; // truthy
@@ -40,11 +40,11 @@ console.log(Boolean(mphAtStart));
 /* EVALUATING BOOLEAN EXPRESSIONS */
 console.log("\nEvaluating Boolean Expressions");
 
-// TODO: Write boolean expressions and print them to see its return value
+// Write boolean expressions and print them to see its return value
 console.log("Calvin" + "Klein" == "Calvin Klein");
 console.log(true != 0);
 
-// TODO: Check to see if the current speed is high enough for time travel, storing it in the fastEnough variable, then print it
+// Check to see if the current speed is high enough for time travel, storing it in the fastEnough variable, then print it
 let currentSpeed = 65;
 let timeTravelSpeed = 88;
 
@@ -55,11 +55,11 @@ console.log(fastEnough);
 /** LOOSE EQUALITY VS STRICT EQUALITY **/
 console.log("\nLoose Equality vs Strict Equality");
 
-// TODO: Compare "8.0" and 8 using both types of equality and print result
+// Compare "8.0" and 8 using both types of equality and print result
 console.log("8.0" == 8);
 console.log("8" === 8);
 
-// TODO: Compare false and 0 using both types of inequality and print result
+// Compare false and 0 using both types of inequality and print result
 console.log(false != 0);
 console.log(false !== 0);
 
@@ -72,16 +72,16 @@ let expression1 = "Great Scott!" === "Great Scott" + "!";
 let expression2 = 1955 <= 1985;
 let expression3 = 10.04 == "10:04";
 
-// TODO: Use the logical AND operator to determine if all three expressions are true, and print the result
+// Use the logical AND operator to determine if all three expressions are true, and print the result
 console.log(expression1 && expression2 && expression3);
 
-// TODO: Use the logical OR operator to determine if either expression2 or expression3 is true, and print the result
+// Use the logical OR operator to determine if either expression2 or expression3 is true, and print the result
 console.log(expression2 || expression3);
 
-// TODO: Use the logical NOT operator to print the opposite value of expression2
+// Use the logical NOT operator to print the opposite value of expression2
 console.log(!expression2);
 
-// TODO: Use parentheses to print the opposite of expression1 AND expression3
+// Use parentheses to print the opposite of expression1 AND expression3
 console.log(!(expression1 && expression3));
 
 
@@ -90,8 +90,8 @@ console.log("\nConditional Statements");
 
 let hasPlutonium = false;
 
-// TODO: Write an IF statement that prints a message if the DeLorean is fully equipped for time travel in 1985
-// TODO: Add an ELSE statement that will print a different message if it's not ready
+// Write an IF statement that prints a message if the DeLorean is fully equipped for time travel in 1985
+// Add an ELSE statement that will print a different message if it's not ready
 
 if (hasPlutonium) {
 	console.log("\nThe flux capacitor is powered up and the DeLorean is ready to go!")
@@ -99,7 +99,7 @@ if (hasPlutonium) {
 	console.log("\nThe DeLorean won't travel through time without fuel for the nuclear reactor!");
 }
 
-// TODO: Write a series of IF/ELSE statements to determine which year the DeLorean will travel to.
+// Write a series of IF/ELSE statements to determine which year the DeLorean will travel to.
 
 let cableElectrified = false;
 let hasBananaPeels = true;
@@ -114,7 +114,7 @@ if (hasPlutonium) {
 	destinationYear = 2015;
 }
 
-// TODO: If the destination year has been set, print a statement that includes it. Otherwise, print some other statement that says they're not going anywhere (anytime?). Hint: remember what we learned about what happens when conditions are not booleans;
+// If the destination year has been set, print a statement that includes it. Otherwise, print some other statement that says they're not going anywhere (anytime?). Hint: remember what we learned about what happens when conditions are not booleans;
 
 if (destinationYear) {
 	console.log("\nReady to go? You're headed to the year " + destinationYear + "!");
@@ -122,7 +122,7 @@ if (destinationYear) {
 	console.log("\nNo destination date has been set. Do you have the materials you need to power the flux capacitor?");
 }
 
-// TODO: Use nested conditionals to print three different statements — one if the car is going 88 MPH, one if it's lower than 88 MPH, and one if the car didn't start in the first place
+// Use nested conditionals to print three different statements — one if the car is going 88 MPH, one if it's lower than 88 MPH, and one if the car didn't start in the first place
 
 let carHasStarted = true;
 let speed = 72;
