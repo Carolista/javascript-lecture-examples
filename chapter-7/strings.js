23d3f8ef50ee6cc3ebcf8da2676ced0552c3eef6
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-30 10:46:51 -0400

    edited comments

--- chapter-7/strings.js
+++ chapter-7/strings.js
@@ -14,13 +14,13 @@
 
 let theLastTimeLord = " Tha Docktor  "; // um... that's not quite right!
 
-// TODO: Use bracket notation to print the first character of the first word.
+// Use bracket notation to print the first character of the first word.
 console.log(theLastTimeLord[1]);
 
-// TODO: Use bracket notation to print the first character of the last word.
+// Use bracket notation to print the first character of the last word.
 console.log(theLastTimeLord[7]);
 
-// TODO: Try to fix the mistakes in the name by using bracket notation to assign new values to characters at specific indices, then print the whole name to see if it worked.
+// Try to fix the mistakes in the name by using bracket notation to assign new values to characters at specific indices, then print the whole name to see if it worked.
 theLastTimeLord[3] = "e";
 theLastTimeLord[8] = "";
 console.log(theLastTimeLord); // ope, nope
@@ -29,17 +29,17 @@ console.log(theLastTimeLord); // ope, nope
 
 /** THE LENGTH PROPERTY **/
 
-// TODO: Declare a variable
+// Declare a variable
 let dalekWarCry = "Exterminate!";
 
-// TODO: Print the length of the string.
+// Print the length of the string.
 console.log(dalekWarCry.length);
 
-// TODO: Use the length to store the index of the last character in a variable, then print it.
+// Use the length to store the index of the last character in a variable, then print it.
 let lastIndex = dalekWarCry.length - 1;
 console.log(lastIndex);
 
-// TODO: Use bracket notation and the variable you created to print the last character of the string.
+// Use bracket notation and the variable you created to print the last character of the string.
 console.log(dalekWarCry[lastIndex]);
 
 
@@ -49,39 +49,39 @@ console.log(dalekWarCry[lastIndex]);
 let getAMoveOn = "Allons-y!";
 let timeDescription = "Wibbly-wobbly, Timey-wimey";
 
-// TODO: Print getAMoveOn in all caps, then print the stored variable.
+// Print getAMoveOn in all caps, then print the stored variable.
 console.log(getAMoveOn.toUpperCase());
 console.log(getAMoveOn);
 
-// TODO: Print timeDescription in all lowercase, then print the stored variable.
+// Print timeDescription in all lowercase, then print the stored variable.
 console.log(timeDescription.toLowerCase());
 console.log(timeDescription);
 
 
 /* Trim & Replace */
 
-// TODO: Remove the leading and trailing spaces from theLastTimeLord and reassign the returned value to the original variable, then print it.
+// Remove the leading and trailing spaces from theLastTimeLord and reassign the returned value to the original variable, then print it.
 theLastTimeLord = theLastTimeLord.trim();
 console.log(theLastTimeLord);
 
-// TODO: Change the errant a to an e in "Tha", reassigning to the original variable, then print it.
+// Change the errant a to an e in "Tha", reassigning to the original variable, then print it.
 theLastTimeLord = theLastTimeLord.replace("a", "e");
 console.log(theLastTimeLord);
 
 
 /* Finding Characters Within a String */
 
-// TODO: Check to see if there is a t in theLastTimeLord, and print the return value (boolean).
+// Check to see if there is a t in theLastTimeLord, and print the return value (boolean).
 console.log(theLastTimeLord.includes("t"));
 
-// TODO: Get the index of the errant k in the name, store it in a variable, then print it.
+// Get the index of the errant k in the name, store it in a variable, then print it.
 let extraLetter = theLastTimeLord.indexOf("k")
 console.log(extraLetter);
 
 
 /* Create a New String from a Substring */
 
-// TODO: Use .slice() to build the correct string, omitting the k, and reassign it to the original variable. Print the final result.
+// Use .slice() to build the correct string, omitting the k, and reassign it to the original variable. Print the final result.
 theLastTimeLord = theLastTimeLord.slice(0, extraLetter) + theLastTimeLord.slice(extraLetter + 1);
 console.log(theLastTimeLord);
 // SUCCESS!
@@ -97,7 +97,7 @@ let quote1 = "Oi!";
 let quote2 = "The first 19 years Of my life, nothing happened. Nothing at all. Not ever. And then I met a man called The Doctor.";
 let quote3 = "All of space and time he promised me!";
 
-// TODO: Using template literals, combine each quote with the correct character so that they come out in the console formatted like this: 
+// Using template literals, combine each quote with the correct character so that they come out in the console formatted like this: 
 // "I am the Master and you will obey me." - The Master
 console.log(`"${quote1}" - ${donna}`);
 console.log(`"${quote2}" - ${rose}`);
@@ -106,10 +106,10 @@ console.log(`"${quote3}" - ${martha}`);
 
 /* PUT IT ALL TOGETHER */
 
-// TODO: Add the readline-sync library so you can accept user input.
+// Add the readline-sync library so you can accept user input.
 const input = require('readline-sync');
 
-// TODO: Ask the user for a first name
+// Ask the user for a first name
 let firstName = input.question("\nPlease enter your first name:\n");
 
 // The user may not give you their name formatted as you need it to be
@@ -118,7 +118,7 @@ let firstName = input.question("\nPlease enter your first name:\n");
 // Think it through — what order should you do the modifications in?
 // NOTE: There is more than one way to do this!
 
-// TODO: Modify the name to ensure the first letter is capitalized, the remaining letters are lowercase, and no extra spaces are before or after the name.
+// Modify the name to ensure the first letter is capitalized, the remaining letters are lowercase, and no extra spaces are before or after the name.
 
 // SOLUTION #1 - Swap using .replace()
 firstName = firstName.trim();
@@ -129,5 +129,5 @@ firstName = firstName.replace(firstName[0], firstName[0].toUpperCase());
 firstName = firstName.trim();
 firstName = firstName[0].toUpperCase() + firstName.slice(1).toLowerCase();
 
-// TODO: Use a template literal to print a greeting with the name
+// Use a template literal to print a greeting with the name
 console.log(`Hello, ${firstName}!`);
