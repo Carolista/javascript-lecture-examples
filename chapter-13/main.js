387862e08c5d585ce54618129398804b3fc95bd1
Carolista <cjones.renaissancewoman@gmail.com>
2023-12-11 17:13:52 -0500

    updated destructuring language in chapter 13 examples

--- chapter-13/main.js
+++ chapter-13/main.js
@@ -9,41 +9,37 @@
 	https://youtube.com/@CodeWithCarrie
 */
 
-
 // NOTE: Import statements usually go together at the top but I will add them gradually below
-
 
 /** EXPORT/IMPORT A SINGLE ITEM **/
 
-// Import the function from contact.js that prints LaunchCode's organizational information, but give it a different name 
+// Import the function from contact.js that prints LaunchCode's organizational information, but give it a different name
 const printLCInfo = require('./common/contact.js');
 
 // Call the function
 printLCInfo();
 
-
 /** EXPORT/IMPORT MULTIPLE ITEMS **/
 
 // Import from utils.js using an alias for the exports object
-const utils = require("./common/utils.js");
+const utils = require('./common/utils.js');
 
 let longDecNum = 43.624571248419348;
 
 // Use the rounding function in utils to print the rounded number to 3 decimal places
 console.log(utils.roundToDec(longDecNum, 3));
 
+/** USING DESTRUCTURING FOR IMPORTS **/
 
-/** USING DECONSTRUCTION FOR IMPORTS **/
+// Use destructuring to import only the last two functions from utils.js
+const { getRandNum, getTitleCase } = require('./common/utils.js');
 
-// Use deconstruction to import only the last two functions from utils.js
-const { getRandNum, getTitleCase } = require("./common/utils.js");
-
-// Use deconstruction to import the question function from the readline-sync library
-const { question } = require("readline-sync");
+// Use destructuring to import the question function from the readline-sync library
+const { question } = require('readline-sync');
 
 // Print a random integer between 0 and 100
 console.log(getRandNum(100));
 
 // Ask a user for a book title, then print it in title case using the third function from utils.js
-let fullName = question("What is one of your favorite books? ");
+let fullName = question('What is one of your favorite books? ');
 console.log(getTitleCase(fullName));
