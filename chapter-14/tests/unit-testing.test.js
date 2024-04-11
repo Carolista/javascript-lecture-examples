07bb4b777a6c0bf054f5253921a7b3af85a0d2e7
Carolista <cjones.renaissancewoman@gmail.com>
2024-04-11 15:45:39 -0400

    updated solution for ch 14 again

--- chapter-14/tests/unit-testing.test.js
+++ chapter-14/tests/unit-testing.test.js
@@ -1,6 +1,5 @@
 // Import items to test
-const examples = require("../unit-testing");
-
+const examples = require('../unit-testing');
 
 /** UNIT TESTING **/
 
@@ -9,10 +8,10 @@ const examples = require("../unit-testing");
 */
 
 // Create a describe block, then write a specification inside it
-describe("The MS_PER_YEAR constant", function () {
-	test("should have a value of 31557600000", function () {
-		expect(examples.MS_PER_YEAR).toEqual(31557600000);
-	});
+describe('The MS_PER_YEAR constant', function () {
+  test('should have a value of 31557600000', function () {
+    expect(examples.MS_PER_YEAR).toEqual(31557600000);
+  });
 });
 
 /*
@@ -20,40 +19,39 @@ describe("The MS_PER_YEAR constant", function () {
 */
 
 // Create a describe block for the entire object
-describe("The bucky object", function () {
-	// Write each specification as a test block inside the describe block
-	
-	// Test the value of fullName
-	test("should have a property called 'fullName' with the value of 'James Buchanan Barnes'", function () {
-		expect(examples.bucky.fullName).toEqual("James Buchanan Barnes");
-	});
-	
-	// Write 2 tests for the value of nickname — it should be "Bucky" and NOT "The Winter Soldier"
-	test("should have a property called 'nickname' with the value of 'Bucky'", function () {
-		expect(examples.bucky.nickname).toEqual("Bucky");
-		expect(examples.bucky.nickname).not.toEqual("The Winter Soldier");
-	});
+describe('The bucky object', function () {
+  // Write each specification as a test block inside the describe block
 
-	// Test that triggerWords includes "longing", "daybreak", and "homecoming"
-	test("should have an array called 'triggerWords' that includes specific words", function () {
-		expect(examples.bucky.triggerWords).toContain("longing");
-		expect(examples.bucky.triggerWords).toContain("daybreak");
-		expect(examples.bucky.triggerWords).toContain("homecoming");
-	});
+  // Test the value of fullName
+  test("should have a property called 'fullName' with the value of 'James Buchanan Barnes'", function () {
+    expect(examples.bucky.fullName).toEqual('James Buchanan Barnes');
+  });
 
-	// Test that the age property is greater than 100
-	test("should have a property called 'age' that is greater than 100 and less than 120", function () {
-		// console.log(bucky.age);
-		expect(examples.bucky.age).toBeGreaterThan(100);
-		expect(examples.bucky.age).toBeLessThan(120);
-	});
+  // Write 2 tests for the value of nickname — it should be "Bucky" and NOT "The Winter Soldier"
+  test("should have a property called 'nickname' with the value of 'Bucky'", function () {
+    expect(examples.bucky.nickname).toEqual('Bucky');
+    expect(examples.bucky.nickname).not.toEqual('The Winter Soldier');
+  });
 
-	// Test that Bucky is no longer a Hydra agent
-	test("should have a property 'isHydraAgent' that is false", function () {
-		expect(examples.bucky.isHydraAgent).toBe(false);
-	});
+  // Test that triggerWords includes "longing", "daybreak", and "homecoming"
+  test("should have an array called 'triggerWords' that includes specific words", function () {
+    expect(examples.bucky.triggerWords).toContain('longing');
+    expect(examples.bucky.triggerWords).toContain('daybreak');
+    expect(examples.bucky.triggerWords).toContain('homecoming');
+  });
+
+  // Test that the age property is greater than 100
+  test("should have a property called 'age' that is greater than 100 and less than 120", function () {
+    // console.log(bucky.age);
+    expect(examples.bucky.age).toBeGreaterThan(100);
+    expect(examples.bucky.age).toBeLessThan(120);
+  });
+
+  // Test that Bucky is no longer a Hydra agent
+  test("should have a property 'isHydraAgent' that is false", function () {
+    expect(examples.bucky.isHydraAgent).toEqual(false);
+  });
 });
-
 
 /** TEST-DRIVEN DEVELOPMENT **/
 
@@ -61,49 +59,59 @@ describe("The bucky object", function () {
 	Write a new describe block for the isAlphaOnly function. Test positive cases, negative cases, and edge cases
 */
 
-// Test data — will need to be moved into a beforeEach() function 
+// Test data
 let cap;
+
+// Reset data before each test in case it is manipulated
 beforeEach(function () {
-	cap = {
-		fullName: "Steve Rogers",
-		nickname: "Cap",
-		age: Math.round((new Date() - new Date(1918, 7, 4)) / examples.MS_PER_YEAR),
-		catchphrases: ["I could do this all day.", "Language!"]
-	};
+  cap = {
+    fullName: 'Steve Rogers',
+    nickname: 'Cap',
+    catchphrases: ['I could do this all day.', 'Language!'],
+    calculateAge: function () {
+      return Math.round((new Date() - new Date(1918, 7, 4)) / examples.MS_PER_YEAR);
+    }
+  };
 });
 
 // Write a describe block for the isAlphaOnly function
-describe("The isAlphaOnly function", function() {
-	
-	// POSITIVE CASES
-	// Write a test for nickname
-	test("should return true if alpha-only string is passed in", function() {
-		expect(examples.isAlphaOnly(cap.nickname)).toBe(true);
-	});
-	
-	// NEGATIVE CASES
-	// Write tests for non-alpha characters like numbers and punctuation
-	test("should return false if the string has numeric characters", function() {
-    cap.age = String(cap.age);
-		expect(examples.isAlphaOnly(cap.age)).toBe(false);
-	});
-	test("should return false if the string has punctuation", function() {
-		expect(examples.isAlphaOnly(cap.catchphrases[0])).toBe(false);
-		expect(examples.isAlphaOnly(cap.catchphrases[1])).toBe(false);
-	});
-	
-	// EDGE CASES
-	// Write a test for spaces (is allowed)
-	test("should return true if the string has spaces", function() {
-		expect(examples.isAlphaOnly(cap.fullName)).toBe(true);
-	});
-	// Write a test for empty string (not allowed)
-	test("should return false if it is an empty string", function() {
-		expect(examples.isAlphaOnly("")).toBe(false);
-	});	
-	// Write a test for non-string types (not allowed)
-	test("should return false if value is not a string type", function() {
-    expect(typeof cap.age).toBe("number");
-		expect(examples.isAlphaOnly(cap.age)).toBe(false);
-	});
+describe('The isAlphaOnly function', function () {
+  // POSITIVE CASES
+  // Write a test for nickname
+  test('should return true if alpha-only string is passed in', function () {
+    expect(examples.isAlphaOnly(cap.nickname)).toEqual(true);
+  });
+  // Write a test for spaces (is allowed)
+  test('should return true if the string has spaces', function () {
+    expect(examples.isAlphaOnly(cap.fullName)).toEqual(true);
+  });
+
+  // NEGATIVE CASES
+  // Write tests for non-alpha characters like numbers and punctuation
+  test('should return false if the string has numeric characters', function () {
+    let numString = String(cap.calculateAge());
+    expect(examples.isAlphaOnly(numString)).toEqual(false);
+  });
+  test('should return false if the string has punctuation', function () {
+    expect(examples.isAlphaOnly(cap.catchphrases[0])).toEqual(false);
+    expect(examples.isAlphaOnly(cap.catchphrases[1])).toEqual(false);
+  });
+
+  // EDGE CASES
+  // Write a test for empty string (not allowed)
+  test('should return false if it is an empty string', function () {
+    expect(examples.isAlphaOnly('')).toEqual(false);
+  });
+  // Write a test for ONLY spaces (no other characters)
+  test('should return true if the string has ONLY spaces', function () {
+    expect(examples.isAlphaOnly(" ")).toEqual(true);
+  });
+  // Write a test for non-string types (not allowed)
+  test('should return false if value is not a string type', function () {
+    expect(examples.isAlphaOnly(cap.calculateAge())).toEqual(false); // number
+    expect(examples.isAlphaOnly(cap.catchphrases)).toEqual(false); // array
+    expect(examples.isAlphaOnly(cap)).toEqual(false); // object
+    expect(examples.isAlphaOnly(cap.calculateAge)).toEqual(false); // function
+    expect(examples.isAlphaOnly(null)).toEqual(false); // null
+  });
 });
